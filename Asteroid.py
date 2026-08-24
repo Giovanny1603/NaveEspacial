0e1e79dae039ec428ffd9f3bfd5d4619d5ed8d5a
CHAGSy <116753597+Giovanny1603@users.noreply.github.com>
2026-08-24 08:55:13 -0300

    Add Asteroid class

---
+++ Asteroid.py
@@ -0,0 +1,33 @@
+import random
+import pygame
+from ElementoJogo import ElementoJogo
+
+class Asteroid(ElementoJogo):
+    def __init__(self, largura_tela, altura_tela, velocidade=5, cor=(200, 50, 50)):
+        self.largura_tela = largura_tela
+        self.altura_tela = altura_tela
+        self.raio = 20
+
+        super().__init__(
+            x=0,
+            y=0,
+            largura=self.raio * 2,
+            altura=self.raio * 2,
+            cor=cor,
+            velocidade=velocidade
+        )
+        self.iniciar_status()
+
+    def iniciar_status(self):
+        # TODO 3 Resolvido: Nascimento aleatório do asteroide
+        self.rect.x = random.randint(0, self.largura_tela - self.rect.width)
+        self.rect.y = random.randint(-150, -50)
+        self.velocidade = random.randint(3, 7)
+
+    def mover(self):
+        self.rect.y += self.velocidade
+        if self.rect.top > self.altura_tela:
+            self.iniciar_status()
+
+    def desenhar(self, tela):
+        pygame.draw.circle(tela, self.cor, self.rect.center, self.raio)
